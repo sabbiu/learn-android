4ee1711111661eb438a8225494263fdae33d484b
sabbiu <070bct531@ioe.edu.np>
2017-09-30 08:55:01 +0545

    App Initialized for Database work

--- app/src/main/java/com/example/appbar/MainActivity.java
+++ app/src/main/java/com/example/appbar/MainActivity.java
@@ -1,40 +1,13 @@
 package com.example.appbar;
 
-import android.content.Intent;
-import android.os.PersistableBundle;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity implements WorkoutListFragment.Listener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
-
-
-    @Override
-    public void itemClicked(long id) {
-        View fragmentContainer = findViewById(R.id.fragment_container);
-        if (fragmentContainer != null) {
-            WorkoutDetailFragment details = new WorkoutDetailFragment();
-            FragmentTransaction fragmentTransaction =
-                    getSupportFragmentManager().beginTransaction();
-            details.setWorkout(id);
-            fragmentTransaction.replace(R.id.fragment_container, details);
-            fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-            fragmentTransaction.addToBackStack(null);
-            fragmentTransaction.commit();
-
-        } else {
-            Intent intent = new Intent(this, DetailActivity.class);
-            intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int) id);
-            startActivity(intent);
-        }
-    }
-
 }
